92ace15a96b93afb51a31b47cf01e9189ddcaa8c
Nuno Custódio <nuno.fcustodio@gmail.com>
2020-12-15 17:43:37 +0000

    .

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-import { Client } from 'discord.js';
-const client = new Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 require('dotenv').config();
 
-client.login(process.env.TOKEN);
+client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
   console.log(`Logged in as 🍕🥓 ${client.user.tag}!`);
 });
+client.on('message', message => {
+    // If the message is "ping"
+    if (message.content === 'ping') {
+      // Send "pong" to the same channel
+      message.channel.send('pong');
+    }
+  });
