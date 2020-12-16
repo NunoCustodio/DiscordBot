6ef5a65b286a7a8d294b8b4f0e181bcd4045ca42
Nuno Custódio <nuno.fcustodio@gmail.com>
2020-12-16 00:17:46 +0000

    .

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
+//const fs = require('fs');
+//const dispatcher = connection.play('/sounds/coca.mp3');
 
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
   console.log(`Logged in as 🍕🥓 ${client.user.tag}!`);
 });
-client.on('message', message => {
-    // If the message is "ping"
-    if (message.content === 'ping') {
-      // Send "pong" to the same channel
-      message.channel.send('pong');
+client.on('message', async (message) => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  } else {
+    if (message.content === 'flour?') {
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.play('./sounds/coca.mp3');
+        dispatcher.on('start', () => {
+          dispatcher.setVolume(1);
+        });
+
+        dispatcher.on('error', (err) => console.log(err));
+
+        dispatcher.on('finish', () => {
+          message.member.voice.channel.leave();
+        });
+      }
     }
-  });
+  }
+});
